df82aec412870df6635f330fae615e71fbed43fa
bsabillon <38368514+bsabillon@users.noreply.github.com>
2019-09-22 01:09:02 -0600

    initial commit

---
+++ routes/companies.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+//const appServices = require('../applicationServices/bookApplicationServices');
+
+
+
+
+//  router.post('/add',appServices.addBook);
+//  router.get('/',appServices.getBooks);
+//  router.get('/byId/:id?',appServices.getBookById);
+//  router.put('/update/:id?',appServices.updateBook);
+//  router.delete('/delete/:id?',appServices.deleteBook);
+
+
+module.exports = router;
